a52d4b5b3f2149a3309505f8e0cea5cc98809dee
alireza <chitsaz.sci@gmail.com>
2024-06-09 20:07:37 -0400

    add the mkdir&clear history of febio run

--- src/mkdirs.c
+++ src/mkdirs.c
@@ -4,15 +4,23 @@
 #include "common.h"
 
 int mkdirs(){
-    strcpy(rundir,"/runfebio/");
-    strcpy(datadir,"/data/");
-
+    strcpy(rundir,"runfebio/");
+    strcpy(datadir,"data/");
+    char command[50]="mkdir ";
+    strcat(command,rundir);
+    system(command);
     strcat(rundir,filename);
     strcat(rundir,slash);
+    strcpy(command,"rm -r ");
+    strcat(command,rundir);
+    system(command);
+
+    strcpy(command,"mkdir ");
+    strcat(command,rundir);
+    system(command);
+
     strcat(datadir,filename);
     strcat(datadir,slash);
-
-    printf("run: %s\ndata: %s\n",rundir,datadir);
 
     return 0;
 }
