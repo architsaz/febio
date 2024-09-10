a5ab7d8bf6f6837bc35a8417f5b06bc59e9666e8
achitsaz <chitsaz.sci@gmail.com>
2024-09-09 21:51:48 -0400

    fixed bug for add ishtar machine in the queue for run code

--- queue.sh
+++ queue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 code_name=febio4
 # List of machines
-machines=("loki" "hades" "marduk" "heise" "attila")
+machines=("ishtar" )
 # List of cases
 cases=($(cat cases.txt)) 
 # Track the number of cases completed
@@ -14,15 +14,26 @@ run_case_on_machine () {
     local dir_name=test
     local code_dir=/dagon1/achitsaz/FEBio
     echo "Running $2 on machine $1"
-    ssh $1 "cd $code_dir && /dagon1/achitsaz/FEBio/scripts/mkrdata.sh $2 $dir_name"
-    ssh $1 "cd $code_dir/$dir_name/$2/msa.1/ && nohup ./run.sh $2 nocorr 0 0 > run.log 2>&1 &" &
+    if [ $1 == "ishtar" ]; then
+        cd $code_dir 
+        /dagon1/achitsaz/FEBio/scripts/mkrdata.sh $2 $dir_name
+        cd $code_dir/$dir_name/$2/msa.1/ 
+        nohup ./run.sh $2 nocorr 0 0 > run.log 2>&1 &
+    else
+        ssh $1 "cd $code_dir && /dagon1/achitsaz/FEBio/scripts/mkrdata.sh $2 $dir_name"
+        ssh $1 "cd $code_dir/$dir_name/$2/msa.1/ && nohup ./run.sh $2 nocorr 0 0 > run.log 2>&1 &" &
+    fi 
 }
 
 while [ $completed_cases -lt $total_cases ]; do
     for machine in "${machines[@]}"; do
         echo "check machine $machine"
         #check if the machine run a task
-        running_task=$(ssh $machine "pgrep febio4 | wc -l")
+        if [ $machine == "ishtar" ]; then
+            running_task=$(pgrep febio4 | wc -l)
+        else
+            running_task=$(ssh $machine "pgrep febio4 | wc -l")
+        fi
         echo "running_task: $running_task"
         if [ "$running_task" -eq 0 ] && [ ${#cases[@]} -gt 0 ]; then
             case=${cases[0]}
