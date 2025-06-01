1e9b37d069c19adcf8f5d436928c925a7653d400
achitsaz <chitsaz.sci@gmail.com>
2025-06-01 10:50:37 -0400

    updated the queue script to the new version

--- queue.sh
+++ queue.sh
@@ -1,63 +1,83 @@
 #!/bin/bash
+
 code_name=febio4
-# List of machines
-machines=("ishtar" "loki" "hades" "attila" "heise")
-# List of cases
-lists_cases_dir=/dagon1/achitsaz/runfebio/all_labeled_cases.txt
+machines=("ishtar" "loki" "hades" "attila" "marduk" "heise")
+# machines=("ishtar")
 
-if [ -f "$lists_cases_dir" ]; then 
-    cases=($(cat "$lists_cases_dir"))
+list_dir=/dagon1/achitsaz/runfebio/problematic_run_cases.txt
+use_test_case=true  # Set to false to read from file
+
+if [ "$use_test_case" = true ]; then
+    raw_cases=("agh111.1")
 else
-    echo "ERROR: File does not exist at: $lists_cases_dir"
-    exit 1
+    if [ -f "$list_dir" ]; then 
+        mapfile -t raw_cases < "$list_dir"
+    else
+        echo "ERROR: Case list not found at: $list_dir"
+        exit 1
+    fi
 fi
- 
-completed_cases=0
+# Generate 4 jobs per case
+jobs=()
+for case in "${raw_cases[@]}"; do
+    jobs+=("$case 1 ")
+    jobs+=("$case 2 ")
 
-total_cases=${#cases[@]}
-echo "Total # cases in the list: $total_cases"
-echo "Total # of available mashines: ${#machines[@]}"
+done
+
+total_jobs=${#jobs[@]}
+echo "Total # of jobs to run: $total_jobs"
+echo "Total # of machines: ${#machines[@]}"
 
 run_case_on_machine () {
-    local dir_name=/dagon1/achitsaz/runfebio
-    echo "-> Running $2 on machine $1"
-    if [ $1 == "ishtar" ]; then
-        # cd $code_dir 
-        # /dagon1/achitsaz/FEBio/scripts.feb/mkrdata.sh $2 $dir_name
-        cd $dir_name/$2/msa.2/ 
-        nohup ./run.sh $2 nocorr 0 0 > run.log 2>&1 &
+    local machine=$1
+    local case_id=$2
+    local msa=$3
+    
+    local mkdata_dir=/dagon1/achitsaz/FEBio/scripts.feb/mkrdata.sh 
+    local case_dir=/dagon1/achitsaz/runfebio
+    local code_dir=/dagon1/achitsaz/mylib/EXECs/
+
+    local command="$mkdata_dir $case_id $case_dir && cd $case_dir/$case_id/msa.$msa/ && nohup ./run.sh $case_id nocorr 0 0 > run.log 2>&1 &"
+    # local command="$mkdata_dir $case_id $case_dir"
+
+    echo "-> Dispatching: $case_id with pst.$msa and $colormap on $machine"
+
+    if [ "$machine" == "ishtar" ]; then
+        eval "$command"
     else
-        # ssh $1 "/dagon1/achitsaz/FEBio/scripts.feb/mkrdata.sh $2 $dir_name"
-        ssh $1 "cd $dir_name/$2/msa.2/ && nohup ./run.sh $2 nocorr 0 0 > run.log 2>&1 &" &
-    fi 
+        ssh "$machine" "$command" &
+    fi
 }
 
-while [ $completed_cases -lt $total_cases ]; do
-    echo "* starting to find a available machine for case : ${cases[0]}"
-    find_machine=0
+while [ ${#jobs[@]} -gt 0 ]; do
+    echo "* Waiting for machine availability..."
+    machine_found=0
+
     for machine in "${machines[@]}"; do
-        #check if the machine run a task
-        if [ $machine == "ishtar" ]; then
+        if [ "$machine" == "ishtar" ]; then
             running_task=$(pgrep febio4 | wc -l)
         else
-            running_task=$(ssh $machine "pgrep febio4 | wc -l")
+            running_task=$(ssh "$machine" "pgrep febio4 | wc -l")
         fi
-        if [ "$running_task" -eq 0 ] && [ ${#cases[@]} -gt 0 ]; then
-            find_machine=1
-            case=${cases[0]}
-            cases=("${cases[@]:1}")
-            run_case_on_machine "$machine" "$case"
-            if [ ${#cases[@]} -eq 0 ]; then
-                echo "All cases completed!"
-                exit
-            fi 
+
+        if [ "$running_task" -eq 0 ]; then
+            machine_found=1
+
+            # Get and remove first job
+            IFS=' ' read -r case_id msa <<< "${jobs[0]}"
+            jobs=("${jobs[@]:1}")
+
+            run_case_on_machine "$machine" "$case_id" "$msa" 
+            break
         fi
-    done    
-    completed_cases=$(($total_cases-${#cases[@]}))
-    if [ $find_machine -eq 0 ]; then
+    done
+
+    if [ $machine_found -eq 0 ]; then
         sleep 1m
     else
         sleep 10
-    fi    
+    fi
 done
 
+echo "✅ All jobs dispatched!"
